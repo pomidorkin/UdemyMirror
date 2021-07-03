0782c0349998ed23188afa5c58e25fbd4b08e427
Pomidorkin <pbriuzgin@gmail.com>
2021-07-03 17:26:45 +0900

    Player mesh renderer update logic added

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,15 +1,21 @@
 using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar]
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
+
+    [SyncVar(hook =nameof(HandleDisplayNameTextUpdate))]
     [SerializeField]
     private string displayName = "Missing name";
 
-    [SyncVar]
+    // hook is going to look for a method and run it.
+    // Whenever playerColor get updated on a client, the fuction we scpecified gets called (thanks to hook)
+    [SyncVar(hook=nameof(HandleDisplayColorUpdate))]
     [SerializeField]
     private Color playerColor = Color.black;
 
@@ -24,5 +30,15 @@ public class MyNetworkPlayer : NetworkBehaviour
     public void SetColor(Color newDisplayColor)
     {
         playerColor = newDisplayColor;
+    }
+
+    private void HandleDisplayColorUpdate(Color oldColor, Color newColor)
+    {
+        displayColorRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    private void HandleDisplayNameTextUpdate(string oldName, string newName)
+    {
+        displayNameText.text = newName;
     }
 }
